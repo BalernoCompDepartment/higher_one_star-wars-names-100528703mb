f593023c46856870e5da1e8d463cb30707bc108c
100528703mb <100528703@schools.edin.org>
2021-01-13 14:17:28 +0000

    finished the program

--- Main.java
+++ Main.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("Enter forename, surname, maiden name and city");
+
+    String forename = input.nextLine();
+    String surname = input.nextLine();
+    String maidenName = input.nextLine();
+    String city = input.nextLine();
+
+    String a =  forename.substring(0,3).toLowerCase();
+    String b =  surname.substring(0,2).toLowerCase();
+    String c =  maidenName.substring(0,3).toLowerCase();
+    String d =  city.substring(0,2).toLowerCase();
+
+    String starWarsName = a + b + c + d;
+
+    System.out.println("Your star wars name is " + starWarsName);
+
+
+  
   }
 }
